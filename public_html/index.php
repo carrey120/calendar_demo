543063c099a8586b8e66360b893cb2de292f0345
Carrey Cheang <carrey.120.cc@gmail.com>
2020-06-05 21:11:10 +0800

    info-panel 架構

--- public_html/index.php
+++ public_html/index.php
@@ -29,6 +29,38 @@
     </div>
 </div>
 
+<div id="info-panel">
+    <div class="close">x</div>
+    <div class="title">
+        <label>Event</label>
+        <div contenteditable="true"></div>
+    </div>
+    <div class="time-picker">
+        <div class="selected-data">
+            <span class="month">10</span>/
+            <span class="date">20</span>
+        </div>
+        <div class="from">
+            <label for="from">from</label>
+            <input id="from" type="time" name="start_time">
+        </div>
+        <div class="to">
+            <label for="to">from</label>
+            <input id="to" type="time" name="end_time">
+        </div>
+    </div>
+    <div class="description">
+        <label>description</label>
+        <div id="description" contenteditable="true"></div>
+    </div>
+    <div class="buttons">
+        <button class="create">create</button>
+        <button class="update">update</button>
+        <button class="cancel">cancel</button>
+        <button class="delete">delete</button>
+    </div>
+</div>
+
 
 
 
