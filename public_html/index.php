7f102956d389264a303acd2a596185712b0935ca
Carrey Cheang <carrey.120.cc@gmail.com>
2020-06-05 21:31:54 +0800

    panel - 1

--- public_html/index.php
+++ public_html/index.php
@@ -32,25 +32,26 @@
 <div id="info-panel">
     <div class="close">x</div>
     <div class="title">
-        <label>Event</label>
+        <label>event</label>
         <div contenteditable="true"></div>
     </div>
     <div class="time-picker">
-        <div class="selected-data">
+        <div class="selected-date">
             <span class="month">10</span>/
             <span class="date">20</span>
         </div>
         <div class="from">
-            <label for="from">from</label>
+            <label for="from">from</label><br>
             <input id="from" type="time" name="start_time">
         </div>
         <div class="to">
-            <label for="to">from</label>
+            <label for="to">from</label><br>
             <input id="to" type="time" name="end_time">
         </div>
     </div>
     <div class="description">
-        <label>description</label>
+        <label>description</label><br>
+        <textarea name="description" id="description" ></textarea>
         <div id="description" contenteditable="true"></div>
     </div>
     <div class="buttons">
